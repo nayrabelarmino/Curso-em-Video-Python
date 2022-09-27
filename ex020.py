cf84a1ab27fccf1c72c27885ff733abdd90914b8
Nayra Belarmino <111246981+nayrabelarmino@users.noreply.github.com>
2022-09-27 16:08:01 -0300

    Update ex020.py

--- ex020.py
+++ ex020.py
@@ -1,3 +1,6 @@
+#O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. 
+#Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
 from random import shuffle
 n1 = str(input('Primeiro aluno: '))
 n2 = str(input('Segundo aluno: '))
@@ -7,9 +10,3 @@ lista = [n1, n2, n3, n4]
 shuffle(lista)
 print('A ordem de apresentação será: ')
 print(lista)
-
-
-
-
-
-
