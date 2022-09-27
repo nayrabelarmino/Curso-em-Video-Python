da338369ecf1e12077a24c9196487b5330132496
Nayra Belarmino <111246981+nayrabelarmino@users.noreply.github.com>
2022-09-27 16:13:02 -0300

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,3 +1,8 @@
+#Crie um programa que leia o nome completo de uma pessoa e mostre: 
+#O nome com todas as letras maiúsculas e minúsculas.
+#Quantas letras ao todo (sem considerar espaços).
+#Quantas letras tem o primeiro nome.
+
 nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome...')
 print('Seu nome em maiúsculas é {}'.format(nome.upper()))
@@ -8,4 +13,3 @@ print('Seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
 
 separa = nome.split()
 print('Seu primeiro nome é {} e tem {} letras'.format(separa[0], len(separa[0])))
-
