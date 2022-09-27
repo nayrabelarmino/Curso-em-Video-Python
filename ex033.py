6df68ba7fc7c7db640af643175cb70cc7586c8f0
Nayra Belarmino <111246981+nayrabelarmino@users.noreply.github.com>
2022-09-27 16:32:57 -0300

    Update ex033.py

--- ex033.py
+++ ex033.py
@@ -1,3 +1,5 @@
+#Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
 a = int(input('Primeiro valor: '))
 b = int(input('Segundo valor: '))
 c = int(input('Terceiro valor: '))
@@ -15,4 +17,3 @@ if c>a and c>b:
     maior = c
 print('O menor valor digitado foi {}'.format(menor))
 print('O maior valor digitado foi {}'.format(maior))
-
