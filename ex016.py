7c6a55f564412de91b978d33dc2d75028052ebc1
Nayra Belarmino <111246981+nayrabelarmino@users.noreply.github.com>
2022-09-27 16:01:36 -0300

    Update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,3 +1,8 @@
+#Crie um programa que leira um número Real qualquer pelo teclado e mostre na tela sua porção inteira.
+
+#Ex: Digite um número: 6.127
+#O número 6.127 tem a parte inteira 6.
+
 '''import math
 num = float(input('Digite um valor: '))
 inteiro = math.floor(num)
