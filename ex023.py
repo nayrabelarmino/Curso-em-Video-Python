32557fdbfd435018dc43bb193f2d76baa16e9454
Nayra Belarmino <111246981+nayrabelarmino@users.noreply.github.com>
2022-09-27 16:15:58 -0300

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -1,3 +1,7 @@
+#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+#Ex: Digite um número: 1834
+#Unidade: 4   Dezena: 3    Centena: 8   Milhar: 1
+
 num = int(input('Digite um número de 0 a 9999: '))
 print('Analisando seu número...')
 u = num // 1 % 10
@@ -9,5 +13,3 @@ print('Unidade: {}'.format(u))
 print('Dezena: {}'.format(d))
 print('Centena: {}'.format(c))
 print('Milhar: {}'.format(m))
-
-
